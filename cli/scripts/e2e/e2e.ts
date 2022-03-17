172846d3b5bffeabcea8e4b93ad1b5bec6888490
Greg Soltis <greg.soltis@vercel.com>
2022-03-17 16:50:35 -0700

    Don't need to edit e2e

--- cli/scripts/e2e/e2e.ts
+++ cli/scripts/e2e/e2e.ts
@@ -102,12 +102,6 @@ function runSmokeTests<T>(
           options
         )
       );
-      // TODO(gsoltis): should we print an english description of the filters used?
-      // assert.fixture(
-      //   `• Packages changed since main: a`,
-      //   sinceCommandOutputNoCache[0],
-      //   "Calculates changed packages (--since)"
-      // );
       assert.fixture(
         sinceCommandOutputNoCache[0],
         `• Packages in scope: a`,
@@ -218,13 +212,13 @@ function runSmokeTests<T>(
       );
 
       assert.fixture(
-        scopeCommandOutput[0],
         `• Packages in scope: a, c`,
+        scopeCommandOutput[0],
         "Packages in scope"
       );
       assert.fixture(
-        scopeCommandOutput[1],
         `• Running test in 2 packages`,
+        scopeCommandOutput[1],
         "Runs only in changed packages"
       );
     }
