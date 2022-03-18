87fb503b954c489bc9a44432bdc0b2a2f25cd554
Greg Soltis <greg.soltis@vercel.com>
2022-03-17 19:45:07 -0700

    Remove unused code

--- cli/internal/scope/scope.go
+++ cli/internal/scope/scope.go
@@ -61,6 +61,28 @@ func (o *Opts) asFilterPatterns() []string {
 	return patterns
 }
 
+func ResolvePackages(opts *Opts, scm scm.SCM, ctx *context.Context, tui cli.Ui, logger hclog.Logger) (util.Set, error) {
+	filterResolver := &scope_filter.Resolver{
+		Graph:                &ctx.TopologicalGraph,
+		PackageInfos:         ctx.PackageInfos,
+		Cwd:                  opts.Cwd,
+		PackagesChangedSince: opts.getPackageChangeFunc(scm, ctx.PackageInfos),
+	}
+	filterPatterns := opts.asFilterPatterns()
+	filteredPkgs, err := filterResolver.GetPackagesFromPatterns(filterPatterns)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(filterPatterns) == 0 {
+		// no filters specified, run every package
+		for _, f := range ctx.PackageNames {
+			filteredPkgs.Add(f)
+		}
+	}
+	return filteredPkgs, nil
+}
+
 func (o *Opts) getPackageChangeFunc(scm scm.SCM, packageInfos map[interface{}]*fs.PackageJSON) scope_filter.PackagesChangedSince {
 	return func(since string) (util.Set, error) {
 		// We could filter changed files at the git level, since it's possible
@@ -84,159 +106,12 @@ func (o *Opts) getPackageChangeFunc(scm scm.SCM, packageInfos map[interface{}]*f
 		if err != nil {
 			return nil, err
 		}
-		changedPkgs := GetChangedPackages(filteredChangedFiles, packageInfos)
+		changedPkgs := getChangedPackages(filteredChangedFiles, packageInfos)
 		return changedPkgs, nil
 	}
 }
 
-func ResolvePackages(opts *Opts, scm scm.SCM, ctx *context.Context, tui cli.Ui, logger hclog.Logger) (util.Set, error) {
-
-	// Note that we do this calculation *before* filtering the changed files.
-	// The user can technically specify both that a file is a global dependency and
-	// that it should be ignored, and currently we treat a change to that file as a
-	// global change.
-	// hasRepoGlobalFileChanged, err := repoGlobalFileHasChanged(opts, changedFiles)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// filteredChangedFiles, err := filterIgnoredFiles(opts, changedFiles)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// changedPackages := make(util.Set)
-	// // Be specific with the changed packages only if no repo-wide changes occurred
-	// if !hasRepoGlobalFileChanged {
-	// 	changedPackages = scope_filter.GetChangedPackages(filteredChangedFiles, ctx.PackageInfos)
-	// }
-
-	// // Scoped packages
-	// // Unwind scope globs
-	// scopePkgs, err := getScopedPackages(ctx.PackageNames, opts.Patterns)
-	// if err != nil {
-	// 	return nil, errors.Wrap(err, "invalid scope")
-	// }
-
-	filterResolver := &scope_filter.Resolver{
-		Graph:                &ctx.TopologicalGraph,
-		PackageInfos:         ctx.PackageInfos,
-		Cwd:                  opts.Cwd,
-		PackagesChangedSince: opts.getPackageChangeFunc(scm, ctx.PackageInfos),
-	}
-	filterPatterns := opts.asFilterPatterns()
-	filteredPkgs, err := filterResolver.GetPackagesFromPatterns(filterPatterns)
-	if err != nil {
-		return nil, err
-	}
-
-	// Filter Packages
-	// filteredPkgs := make(util.Set)
-	// includeDependencies := opts.IncludeDependencies
-	// includeDependents := opts.IncludeDependents
-	// If there has been a global change, run everything in scope
-	// 		(this may be every package if no scope is provider)
-	if len(filterPatterns) == 0 {
-		// no filters specified, run every package
-		for _, f := range ctx.PackageNames {
-			filteredPkgs.Add(f)
-		}
-	}
-	// if hasRepoGlobalFileChanged {
-	// 	// If a global dependency has changed, run everything in scope.
-	// 	// If no scope was provided, run everything
-	// 	if scopePkgs.Len() > 0 {
-	// 		filteredPkgs = scopePkgs
-	// 	} else {
-	// 		for _, f := range ctx.PackageNames {
-	// 			filteredPkgs.Add(f)
-	// 		}
-	// 	}
-	// } else if scopePkgs.Len() > 0 && changedPackages.Len() > 0 {
-	// 	// If we have both a scope and changed packages:
-	// 	// We want the intersection of two sets:
-	// 	// 1. the scopes and all of their dependencies
-	// 	// 2. the changed packages and all of their dependents
-	// 	//
-	// 	// Note that other commandline flags can cause including dependents / dependencies
-	// 	// beyond this set
-
-	// 	// scopes and all deps
-	// 	rootsAndDeps := make(util.Set)
-	// 	for _, pkg := range scopePkgs {
-	// 		rootsAndDeps.Add(pkg)
-	// 		deps, err := ctx.TopologicalGraph.Ancestors(pkg)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 		for _, dep := range deps {
-	// 			rootsAndDeps.Add(dep)
-	// 		}
-	// 	}
-
-	// 	// changed packages and all dependents
-	// 	for _, pkg := range changedPackages {
-	// 		// do the intersection inline, rather than building up the set
-	// 		if rootsAndDeps.Includes(pkg) {
-	// 			filteredPkgs.Add(pkg)
-	// 		}
-	// 		dependents, err := ctx.TopologicalGraph.Descendents(pkg)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 		for _, dependent := range dependents {
-	// 			if rootsAndDeps.Includes(dependent) {
-	// 				filt
-	//         eredPkgs.Add(dependent)
-	// 			}
-	// 		}
-	// 	}
-	// 	tui.Output(fmt.Sprintf(ui.Dim("• Packages changed since %s in scope: %s"), opts.Since, strings.Join(filteredPkgs.UnsafeListOfStrings(), ", ")))
-	// } else if changedPackages.Len() > 0 {
-	// 	// --since was specified, there are changes, but no scope was specified.
-	// 	// Run the packages that have changed
-	// 	filteredPkgs = changedPackages
-	// 	tui.Output(fmt.Sprintf(ui.Dim("• Packages changed since %s: %s"), opts.Since, strings.Join(filteredPkgs.UnsafeListOfStrings(), ", ")))
-	// } else if scopePkgs.Len() > 0 {
-	// 	// There was either a global change, or no changes, or no --since flag
-	// 	// There was a --scope flag, run the desired scopes
-	// 	filteredPkgs = scopePkgs
-	// } else if opts.Since == "" {
-	// 	// No scope was specified, and no diff base was specified
-	// 	// Run every package
-	// 	for _, f := range ctx.PackageNames {
-	// 		filteredPkgs.Add(f)
-	// 	}
-	// }
-
-	// if includeDependents {
-	// 	// TODO(gsoltis): we're concurrently iterating and adding to a map, potentially
-	// 	// resulting in a bunch of duplicate work as we look for descendents of something
-	// 	// that has already had all of its descendents included.
-	// 	for _, pkg := range filteredPkgs {
-	// 		err = addDependents(filteredPkgs, pkg, ctx, logger)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 	}
-	// 	logger.Debug("running with dependents")
-	// }
-
-	// if includeDependencies {
-	// 	// TODO(gsoltis): we're concurrently iterating and adding to a map, potentially
-	// 	// resulting in a bunch of duplicate work as we look for dependencies of something
-	// 	// that has already had all of its dependencies included.
-	// 	for _, pkg := range filteredPkgs {
-	// 		err = addDependencies(filteredPkgs, pkg, ctx, logger)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 	}
-	// 	logger.Debug(ui.Dim("running with dependencies"))
-	// }
-	return filteredPkgs, nil
-}
-
-// Returns platform-dependent paths relative to the root of the monorepo
+// getChangedFiles returns platform-dependent paths relative to the root of the monorepo
 func getChangedFiles(opts *Opts, scm scm.SCM) ([]string, error) {
 	if opts.Since == "" {
 		return []string{}, nil
@@ -276,74 +151,7 @@ func filterIgnoredFiles(opts *Opts, changedFiles []string) ([]string, error) {
 	return filteredChanges, nil
 }
 
-// func addDependents(deps util.Set, pkg interface{}, ctx *context.Context, logger hclog.Logger) error {
-// 	descenders, err := ctx.TopologicalGraph.Descendents(pkg)
-// 	if err != nil {
-// 		return errors.Wrap(err, "error calculating affected packages")
-// 	}
-// 	logger.Debug("dependents", "pkg", pkg, "value", descenders.List())
-// 	for _, d := range descenders {
-// 		// we need to exclude the fake root node
-// 		// since it is not a real package
-// 		if d != ctx.RootNode {
-// 			deps.Add(d)
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func addDependencies(deps util.Set, pkg interface{}, ctx *context.Context, logger hclog.Logger) error {
-// 	ancestors, err := ctx.TopologicalGraph.Ancestors(pkg)
-// 	if err != nil {
-// 		return errors.Wrap(err, "error getting dependency")
-// 	}
-// 	logger.Debug("dependencies", "pkg", pkg, "value", ancestors.List())
-// 	for _, d := range ancestors {
-// 		// we need to exclude the fake root node
-// 		// since it is not a real package
-// 		if d != ctx.RootNode {
-// 			deps.Add(d)
-// 		}
-// 	}
-// 	return nil
-// }
-
-// getScopedPackages returns a set of package names in scope for a given list of glob patterns
-func getScopedPackages(packageNames []string, scopePatterns []string) (util.Set, error) {
-	scopedPkgs := make(util.Set)
-	if len(scopePatterns) == 0 {
-		return scopedPkgs, nil
-	}
-
-	include := make([]string, 0, len(scopePatterns))
-	exclude := make([]string, 0, len(scopePatterns))
-
-	for _, pattern := range scopePatterns {
-		if strings.HasPrefix(pattern, "!") {
-			exclude = append(exclude, pattern[1:])
-		} else {
-			include = append(include, pattern)
-		}
-	}
-
-	glob, err := filter.NewIncludeExcludeFilter(include, exclude)
-	if err != nil {
-		return nil, err
-	}
-	for _, f := range packageNames {
-		if glob.Match(f) {
-			scopedPkgs.Add(f)
-		}
-	}
-
-	if len(include) > 0 && scopedPkgs.Len() == 0 {
-		return nil, errors.Errorf("No packages found matching the provided scope pattern.")
-	}
-
-	return scopedPkgs, nil
-}
-
-func GetChangedPackages(changedFiles []string, packageInfos map[interface{}]*fs.PackageJSON) util.Set {
+func getChangedPackages(changedFiles []string, packageInfos map[interface{}]*fs.PackageJSON) util.Set {
 	changedPackages := make(util.Set)
 	for k, pkgInfo := range packageInfos {
 		partialPath := pkgInfo.Dir
